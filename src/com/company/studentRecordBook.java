873e2492a2169c391ef4dc45401d23dd33ffc98a
Olexandrbebekh <olexandrbebeh@gmail.com>
2020-02-11 21:39:59 +0200

    Add atributes to new class

--- src/com/company/studentRecordBook.java
+++ src/com/company/studentRecordBook.java
@@ -1,4 +1,12 @@
 package com.company;
 
 public class studentRecordBook {
+    public String Surname;
+    public String Forename;
+    public String Fatherland;
+    public int Number;
+    public byte Course;
+    public String[] Exams;
+    public String[] Offset;
+    public float MiddleMark;
 }
