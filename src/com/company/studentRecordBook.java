12e5bbbe3a36f9078384ada5a6a185a5b1f38c49
Olexandrbebekh <olexandrbebeh@gmail.com>
2020-02-11 21:42:28 +0200

    Add constructors to studentRecordBook

--- src/com/company/studentRecordBook.java
+++ src/com/company/studentRecordBook.java
@@ -1,6 +1,7 @@
 package com.company;
 
 public class studentRecordBook {
+
     public String Surname;
     public String Forename;
     public String Fatherland;
@@ -9,4 +10,29 @@ public class studentRecordBook {
     public String[] Exams;
     public String[] Offset;
     public float MiddleMark;
+
+    public studentRecordBook() {
+
+    }
+
+    public studentRecordBook(String surname, String forename, String fatherland, int number, byte course) {
+        Surname = surname;
+        Forename = forename;
+        Fatherland = fatherland;
+        Number = number;
+        Course = course;
+    }
+
+    public studentRecordBook(String surname, String forename, String fatherland, int number, byte course, String[] exams, String[] offset, float middleMark) {
+        Surname = surname;
+        Forename = forename;
+        Fatherland = fatherland;
+        Number = number;
+        Course = course;
+        Exams = exams;
+        Offset = offset;
+        MiddleMark = middleMark;
+    }
+
+
 }
